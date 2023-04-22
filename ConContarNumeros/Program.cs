737181b4554a085f7bfeaedf0e2fe47cee446fd0
allan <allan@LAPTOP-VQUKN846>
2023-04-21 23:08:38 -0400

    Agregar archivos de proyecto.

---
+++ ConContarNumeros/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConContarNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, cont = 0;
+            Console.WriteLine("ingrese numero");
+            int.TryParse(Console.ReadLine(), out numero);
+            while (numero != 0)
+            {
+                cont++;
+                numero = numero / 10;
+            }
+            Console.WriteLine("la cantidad de digitos es de: " + cont);
+        }
+    }
+}
